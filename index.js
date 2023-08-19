469ac9defd40baff62022812401bd287c7725133
armando.alonso <armandoalonso1@gmail.com>
2023-08-19 08:37:41 -0400

    rewrite cli to be both interactive and silent, refactor to to make template generation simple

--- index.js
+++ index.js
@@ -1,19 +1,55 @@
+const {prompt} = require('inquirer');
 const {program} = require('commander');
 const chalk = require('chalk');
-const {plugin} = require('./commands/make');
+const package = require('./package.json'); 
+const scaffold = require('./commands/scaffold');
+
+const questions = [
+    {
+        type: 'list',
+        name: 'addonType',
+        message: 'Select addon type ...',
+        choices: ['plugin', 'behavior', 'effect', 'theme']
+    },
+    {
+        type: 'input',
+        name: 'addonName',
+        message: 'Enter addon name ...'
+    },
+    {
+        type: 'input',
+        name: 'author',
+        message: 'Enter author name ...'
+    }
+];
+
+async function parseArguments(options) {
+    let opts = {};
+    // check if options object contains all required properties
+    if (options.type && options.name && options.author) {
+        opts = { addonType: options.type, addonName: options.name, addonAuthor: options.author };
+    } else {
+        console.log(chalk.green.bold('creating addon ...'));
+        const answers = await prompt(questions);
+        opts = { addonType: answers.addonType, addonName: answers.addonName, addonAuthor: answers.author };
+    }
+    return opts;
+}
 
 program
-.version('0.0.1')
+.version(package.version)
 .description('a cli for generating addon\'s for construct 3 using c3ide2-framework')
-
-program
-.command('plugin <name>')
-.option('-a, --author <author>', 'the author of the plugin', 'default')
-.description('Create a new plugin')
-.action((name) => {
-    const author = program.commands[0]._optionValues.author;
-    console.log(chalk.green(`Creating plugin ${name} by ${author}`));
-    plugin(name, author);
+.option('-t, --type <type>', 'the type of addon to create') // plugin / behavior / effect 
+.option('-n, --name <name>', 'the name of the addon')
+.option('-a, --author <author>', 'the author of the addon')
+.action(async (options) => {
+    const opts = await parseArguments(options);
+    console.log(chalk.green.bold('creating addon ...'));
+    console.log(chalk.green.bold('type: ') + chalk.white.bold(opts.addonType));
+    console.log(chalk.green.bold('name: ') + chalk.white.bold(opts.addonName));
+    console.log(chalk.green.bold('author: ') + chalk.white.bold(opts.addonAuthor));
+  
+    scaffold(opts.addonType, opts);
 });
 
 program.parse(process.argv);
